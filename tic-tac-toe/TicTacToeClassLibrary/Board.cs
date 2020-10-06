d335d813dbf5a895b581b870f1a5fa092fa2ef39
Joel Milligan <22701252@student.uwa.edu.au>
2020-10-06 22:30:42 +0800

    Created empty classes for game state, player, and board

---
+++ tic-tac-toe/TicTacToeClassLibrary/Board.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeClassLibrary
+{
+    class Board
+    {
+    }
+}
--- tic-tac-toe/TicTacToeClassLibrary/Player.cs
+++ tic-tac-toe/TicTacToeClassLibrary/Player.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TicTacToeClassLibrary
 {
-    public class Class1
+    class Player
     {
     }
 }
