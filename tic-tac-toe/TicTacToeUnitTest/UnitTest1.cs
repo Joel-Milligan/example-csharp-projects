fe0ca9ced660576beb51e9ad03dc8539338478f0
Joel Milligan <22701252@student.uwa.edu.au>
2020-10-06 22:24:21 +0800

    Added Unit Test boiler plate

---
+++ tic-tac-toe/TicTacToeUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TicTacToeUnitTest
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
