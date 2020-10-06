d51780512b2c865072b8f38df9c67f5cd8c349c7
Joel Milligan <22701252@student.uwa.edu.au>
2020-10-06 23:47:16 +0800

    Created unit tests for the board class

--- tic-tac-toe/TicTacToeClassLibrary/Board.cs
+++ tic-tac-toe/TicTacToeClassLibrary/Board.cs
@@ -4,7 +4,47 @@ using System.Text;
 
 namespace TicTacToeClassLibrary
 {
-    class Board
+    public class Board
     {
+        public char[,] BoardState { get; set; }
+        public char CurrentPlayer { get; set; }
+
+        // Create an empty board
+        public Board()
+        {
+            BoardState = new char[3, 3];
+            this.EmptyBoard();
+        }
+
+        public void EmptyBoard()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    this.BoardState[row, col] = '\0';
+                }
+            }
+        }
+
+        public void MakeMove(char player, int row, int col)
+        {
+            if(player == 'x' || player == 'o')
+            {
+                if(this.BoardState[row, col] == '\0')
+                {
+                    this.BoardState[row, col] = player;
+                }
+                else
+                {
+                    throw new ArgumentException($"Given cell ({row}, {col}) is not empty.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Given player '{player}' is an invalid character.");
+            }
+        }
     }
 }
+    
--- tic-tac-toe/TicTacToeUnitTest/UnitTests.cs
+++ tic-tac-toe/TicTacToeUnitTest/UnitTests.cs
@@ -1,14 +1,71 @@
 using System;
+using System.Linq;
 using Xunit;
+using TicTacToeClassLibrary;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TicTacToeUnitTest
 {
     public class UnitTests
     {
-        [Fact]
-        public void TrueIsTrue()
+        public bool compare2DArrays(char[,] arr1, char[,] arr2)
         {
-            Assert.True(true);
+            if (arr1.Length != arr2.Length)
+            {
+                return false;
+            }
+            
+            for (int row = 0; row < arr1.GetLength(0); row++)
+            {
+                for (int col = 0; col < arr1.GetLength(1); col++)
+                {
+                    if (arr1[row, col] != arr2[row, col])
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+        [Fact]
+        public void CreateNewBoard()
+        {
+            Board emptyBoard = new Board();
+
+            char[,] emptyState =
+            {
+                {'\0', '\0', '\0'},
+                {'\0', '\0', '\0'},
+                {'\0', '\0', '\0'}
+            };
+
+            Assert.True(compare2DArrays(emptyBoard.BoardState, emptyState));
+        }
+
+        [Fact]  
+        public void InvalidCharMove()
+        {
+            Board board = new Board();
+
+            Assert.Throws<ArgumentException>(
+                () => board.MakeMove('z', 0, 0)
+            );
+
+            board.MakeMove('o', 1, 1);
+            board.MakeMove('x', 2, 2);
+        }
+
+        [Fact]
+        public void MoveIntoNonEmptyCell()
+        {
+            Board board = new Board();
+            board.MakeMove('x', 0, 0);
+
+            Assert.Throws<ArgumentException>(
+                () => board.MakeMove('o', 0, 0)
+            );
         }
     }
 }
