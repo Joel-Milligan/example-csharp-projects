4ed1056a650c2080b19341881036bf938e526a33
thehayman22 <22701252@student.uwa.edu.au>
2020-10-09 19:05:16 +0800

    Added user input for the filepath

--- weather-stats/WeatherStats/Program.cs
+++ weather-stats/WeatherStats/Program.cs
@@ -9,17 +9,39 @@ namespace WeatherStats
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args) // TOOD: Take args for where to place the output file.
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             Client client = new Client();
             await client.RetrieveJSON();
 
             WeatherCollection weatherCollection = JsonConvert.DeserializeObject<WeatherCollection>(client.RawJSONResponse);
 
-            string filePath = @"C:\Users\Public\Test.txt";
-
             FileWriter fileWriter = new FileWriter(weatherCollection);
-            fileWriter.WriteOutputToFile(filePath);
+            
+            string filePath = "";
+
+            if (args.Length == 1)
+            {
+                filePath = args[0];
+            }
+            else if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter path for the output file: ");
+                filePath = Console.ReadLine();
+            } else
+            {
+                Console.WriteLine("Incorrect number of arguments.");
+            }
+
+            try
+            {
+                fileWriter.WriteOutputToFile(filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Path");
+                Console.WriteLine(e);
+            }
         }
     }
 }
